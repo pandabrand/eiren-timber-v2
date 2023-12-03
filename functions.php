6e6ffd36d59de6853ad7297c2292aab23104959a
Frederick Wells <pandabrand@gmail.com>
2023-12-03 16:03:43 -0600

    video page

--- functions.php
+++ functions.php
@@ -21,3 +21,13 @@ add_action( 'wp_enqueue_scripts', 'load_custom_scripts' );
 function load_custom_scripts() {
     wp_enqueue_script( 'app-script', get_template_directory_uri(). '/assets/dist/js/app.js', array(), '1.0.0', true );
 }
+
+add_filter('body_class', function (array $classes) {
+    /** Add page slug if it doesn't exist */
+    if (is_single() || is_page() && !is_front_page()) {
+        if (!in_array(basename(get_permalink()), $classes)) {
+            $classes[] = basename(get_permalink());
+        }
+    }
+    return array_filter($classes);
+});
